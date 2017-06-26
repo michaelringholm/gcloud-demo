65567a99ac57ab2da4a681136e1c31fd0914a0c3
Michael Ringholm Sundgaard <michaelringholm@gmail.com>
2017-06-26 20:10:50 +0200

    Now saving to Google Cloud Datastore via Catatumbo

---
+++ service-registry/src/main/java/com/stelinno/service/ServiceRegistryApp.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.stelinno.service;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jmethods.catatumbo.EntityManager;
+import com.jmethods.catatumbo.EntityManagerFactory;
+import com.stelinno.service.entities.Service;
+
+@SpringBootApplication
+@RestController
+public class ServiceRegistryApp {
+  @RequestMapping("/home")
+  public String home() {
+    return "Service Registry Home!";
+  }
+  
+  /***
+   * curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"Sports Results", "domain":"Sports", "subDomain":"Statistics", "endpoint":"http://sports-service.azure.com"}' http://localhost:8080/register
+   * @param serviceDraft
+   */
+  	@RequestMapping(value="/register", method=RequestMethod.POST)
+	public void register(@RequestBody Service service) {
+		EntityManagerFactory emf = EntityManagerFactory.getInstance();
+		//EntityManager em = emf.createDefaultEntityManager("com.stelinno.uddi");
+		EntityManager em = emf.createEntityManager("stelinno-dev", "Stelinno-DEV-f9c6d1e34d94.json", "com.stelinno.uddi");
+		service = em.insert(service);
+		System.out.println(String.format("service with ID %d created successfully", service.getId()));
+	}
+  	
+  	@RequestMapping(value="/create", method=RequestMethod.GET)
+	public void create(@RequestBody Service serviceDraft) {
+		EntityManagerFactory emf = EntityManagerFactory.getInstance();
+		//EntityManager em = emf.createDefaultEntityManager("com.stelinno.uddi");
+		EntityManager em = emf.createEntityManager("stelinno-dev", "Stelinno-DEV-f9c6d1e34d94.json", "com.stelinno.uddi");
+		
+		Service service = new Service("Sports Results", "Sports", "Statistics", "http://sports-service.azure.com");
+		service = em.insert(service);
+		System.out.println(String.format("service with ID %d created successfully", service.getId()));
+	}  	
+
+  /**
+   * <a href="https://cloud.google.com/appengine/docs/flexible/java/how-instances-are-managed#health_checking">
+   * App Engine health checking</a> requires responding with 200 to {@code /_ah/health}.
+   */
+  @RequestMapping("/_ah/health")
+  public String healthy() {
+    // Message body required though ignored
+    return "Still surviving.";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceRegistryApp.class, args);
+  }
+}
---
+++ service-registry/src/main/java/com/stelinno/service/entities/Service.java
@@ -0,0 +1,75 @@
+package com.stelinno.service.entities;
+
+import com.jmethods.catatumbo.Entity;
+import com.jmethods.catatumbo.Identifier;
+import com.jmethods.catatumbo.Property;
+
+@Entity(kind = "service")
+public class Service {
+
+	public Service() {
+		
+	}
+	
+    public Service(String name, String domain, String subDomain, String endpoint) {
+    	this.name = name;
+    	this.domain = domain;
+    	this.subDomain = subDomain;
+    	this.endpoint = endpoint;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDomain() {
+		return domain;
+	}
+
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+
+	public String getSubDomain() {
+		return subDomain;
+	}
+
+	public void setSubDomain(String subDomain) {
+		this.subDomain = subDomain;
+	}
+
+	public String getEndpoint() {
+		return endpoint;
+	}
+
+	public void setEndpoint(String endpoint) {
+		this.endpoint = endpoint;
+	}
+
+	@Identifier
+    private long id;
+
+    @Property(name = "name")
+    private String name;
+
+    @Property(name = "domain")
+    private String domain;
+
+    @Property(name = "subDomain")
+    private String subDomain;
+    
+    @Property(name = "endpoint")
+    private String endpoint;
+}
